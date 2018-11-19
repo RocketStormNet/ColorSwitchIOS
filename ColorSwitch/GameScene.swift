ecbf5a18f5fd9b531601491edd684bbaa1bd43ee
RocketStormNet <rocketstormnet@outlook.com>
2018-11-19 11:33:01 +0500

    Initial commit

---
+++ ColorSwitch/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  ColorSwitch
+//
+//  Created by Artur Shamsutdinov on 19/11/2018.
+//  Copyright © 2018 rocketstorm. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ ColorSwitch/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  ColorSwitch
+//
+//  Created by Artur Shamsutdinov on 19/11/2018.
+//  Copyright © 2018 rocketstorm. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
